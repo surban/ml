2885348a22d4b3d76b2e991dd6472e16fab1600d
Sebastian Urban <surban@tum.de>
2013-01-07 16:28:30 +0100

    pull

--- pull.cmd
+++ pull.cmd
@@ -1,5 +1,8 @@
 @echo off
+pushd %~dp0
 robocopy \\lily\local\surban\dev\ml\ . /MIR /XD apps /XD .git /NJH /NJS /NDL
 robocopy \\lily\local\surban\dev\ml\apps\ .\apps /PURGE /NJH /NJS /NDL
 robocopy \\lily\local\surban\dev\ml\.git .\.git /PURGE /NJH /NJS /NDL /NFL
+popd
+
 
