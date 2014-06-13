a35421082fb8c47ff2f1e469400b989cec9b05e1
Sebastian Urban <surban@tum.de>
2014-06-13 19:47:51 +0200

    skin job best results

---
+++ ml/apps/skin/cfgs/best/2/cfg.py
@@ -0,0 +1,12 @@
+
+# SUBMIT: runner=python -m ml.apps.skin.conditionalchain_gp
+# SBATCH --mem=3096
+# SBATCH --time=1:00:00
+dataset_name = "bidir_small"
+discrete_force_states = 100
+discrete_skin_states = 50
+kernel = "Matern32"
+std_adjust = 1.5
+cutoff_stds = 1.2
+mngful_dist = 1.0
+normalize_y = False
