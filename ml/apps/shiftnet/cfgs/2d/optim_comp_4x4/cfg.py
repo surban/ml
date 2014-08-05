7ff2aaea5ab33b1dda3c4225872884f62d8df514
Sebastian Urban <surban@tum.de>
2014-08-05 20:03:13 +0200

    cfgs

---
+++ ml/apps/shiftnet/cfgs/2d/optim_comp_4x4/cfg.py
@@ -0,0 +1,18 @@
+# RUNNER:apps.shiftnet.shiftnet_2d
+
+arch = 'comp'
+width = 4
+height = 4
+n_val_samples = 1000
+n_batch = 1000
+optimizer = 'rmsprop'
+init = 0.1
+momentum = 0.8
+new_data_iters = 1000
+max_iters = 0
+min_iters = None
+start_with_optimal_weights = True
+
+steprate_itr = [0,    9000]
+steprate_val = [1e-4, 1e-5]
+
---
+++ ml/apps/shiftnet/cfgs/2d/rmsprop_conv_4x4/cfg.py
@@ -0,0 +1,17 @@
+# RUNNER:apps.shiftnet.shiftnet_2d
+
+arch = 'conv'
+width = 4
+height = 4
+n_val_samples = 1000
+n_batch = 1000
+optimizer = 'rmsprop'
+init = 0.1
+momentum = 0.8
+new_data_iters = 1000
+max_iters = None
+min_iters = 20000
+
+steprate_itr = [0,    9000]
+steprate_val = [1e-4, 1e-5]
+
